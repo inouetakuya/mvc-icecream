dfef9ade14219857d6bf447adcea5cd10088c607
INOUE Takuya <inouetakuya5@gmail.com>
2014-08-04 02:13:45 +0900

    + var icecreamModel = {

---
+++ mvc-icecream.js
@@ -0,0 +1,10 @@
+var icecreamModel = {
+  list: [
+    {id: 'topping1', name: "バニラ"},
+    {id: 'topping2', name: "チョコレートチップ"},
+    {id: 'topping3', name: "オレンジシャーベット"},
+    {id: 'topping4', name: "チョコミント"},
+    {id: 'topping5', name: "ストロベリー"},
+    {id: 'topping6', name: "抹茶"}
+  ]
+};
