85d3e821db046bd78fc10014f73efe10e7211bdc
INOUE Takuya <inouetakuya5@gmail.com>
2014-09-17 18:06:23 +0900

    選択したアイスクリームを表示できるようにした

--- mvc-icecleam.js
+++ mvc-icecleam.js
@@ -7,11 +7,18 @@ $(function () {
   $.each(icecreamModel.getAll(),
     function (i, icecream) {
       els.append($("<li>")
-        .append($("<input type='checkbox'>").attr('name', icecream.id))
-        .append($("<span>").text(icecream.name))
-        .click(function () {
-          // TODO: コントローラ呼び出し
-        })
+          .append($("<input type='checkbox'>").attr('name', icecream.id))
+          .append($("<span>").text(icecream.name))
+          .click(function (event) {
+            var checkbox = $(event.currentTarget).find("input[type='checkbox']");
+            selectionModel.add(icecreamModel.findById(checkbox.attr("name")));
+
+            var selectionText = $.map(selectionModel.getIcecreams(), function (val) {
+              return val.name;
+            }).join(" > ");
+
+            $("#icecream-list").text(selectionText);
+          })
       );
     }
   );
