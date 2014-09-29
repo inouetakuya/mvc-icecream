3e3c80bf5c77e09824a6ace6c25cd42daedfc535
INOUE Takuya <inouetakuya5@gmail.com>
2014-09-29 18:14:53 +0900

    コメントを追加した

--- mvc-icecleam.js
+++ mvc-icecleam.js
@@ -10,11 +10,16 @@ $(function () {
           .append($("<input type='checkbox'>").attr('name', icecream.id))
           .append($("<span>").text(icecream.name))
           .click(function (event) {
+
+            // どのアイスクリームにチェックを入れたのか取得する
             var checkbox = $(event.currentTarget).find("input[type='checkbox']");
+
+            // チェックを入れたアイスクリームをセレクションに追加する
             selectionModel.add(icecreamModel.findById(checkbox.attr("name")));
 
-            var selectionText = $.map(selectionModel.getIcecreams(), function (val) {
-              return val.name;
+            // セレクションを取得して、テキストを表示する
+            var selectionText = $.map(selectionModel.getIcecreams(), function (icecream) {
+              return icecream.name;
             }).join(" > ");
 
             $("#icecream-list").text(selectionText);
